543ca5155db0be42691829772c011d685f8bebf1
robynroby <robinsonmwaura254@gmail.com>
2022-12-26 23:20:04 +0300

    functions

---
+++ cousera/functions and methods/getMax.go
@@ -0,0 +1,21 @@
+package main
+
+import(
+	"fmt"
+)
+
+func getMax(vals ...int) int{
+	maxV := -1
+	for _, v:= range vals{
+		if v > maxV{
+			maxV = v
+		}
+	}
+	return maxV
+}
+
+func main(){
+	fmt.Println(getMax(1,3,6,9,6,89,4))
+	vslice := []int {1,3,6,4}
+	fmt.Println(getMax(vslice ...))
+}
